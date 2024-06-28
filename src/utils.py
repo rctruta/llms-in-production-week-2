81e6f15a974f2ebe49f0072be59efc702f8d7d02
Ramona C Truta <ramona.truta@gmail.com>
2024-06-28 15:57:10 +0000

    Complete homework

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,16 @@ import os
 import streamlit as st
 
 
+def display_defensive_warning() -> None:
+    """
+    Let the user know that the LLM can output incorrect answers.
+    """
+    st.warning("""As an LLM, I cannot guarantee that the SQL code I generated will provide you with the answer you 
+               need for your question. Try to be as precise as possible. Moreover, it would help if you can specify 
+               the schema of your database.
+               """, icon="⚠️")
+
+
 def get_openai_api_key() -> None:
     """
     Get the OpenAI API key from the user.
